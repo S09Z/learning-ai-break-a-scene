41a3f5aefe7385183c3cc03dace575921ec9322e
ittichaib94 <48817606+mantails09@users.noreply.github.com>
2023-09-27 16:22:57 +0700

    update

--- train.py
+++ train.py
@@ -1,18 +1,11 @@
-"""
-Copyright 2023 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# python train.py \
+#   --instance_data_dir examples/toys  \
+#   --num_of_assets 1 \
+#   --initializer_tokens toys \
+#   --class_data_dir inputs/test \
+#   --phase1_train_steps 400 \
+#   --phase2_train_steps 400 \
+#   --output_dir outputs/toys
 
 import argparse
 import hashlib
